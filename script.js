1a9037a998e31241e593e5c405c4366e68eb4ab0
Kocia <annazofka@gmail.com>
2023-10-01 05:35:18 +0100

    rename images, add click events for clickable slideshow icons

---
+++ script.js
@@ -0,0 +1,46 @@
+const $slideshow = document.querySelector('#slideshow');
+const $imagePreviewer = document.querySelector('#image-previewer');
+const $backdrop = document.querySelector('#backdrop');
+const $closeButton = document.querySelector('#close-button');
+const $nextButton = document.querySelector('#next-button');
+const $prevButton = document.querySelector('#prev-button');
+let counter = 1;
+
+const assignImageSource = imageId => {
+	$imagePreviewer.src = `/images/${counter}.jpg`;
+};
+
+const endSlideshow = () => {
+	$backdrop.classList.add('hide');
+};
+
+const openSlideshow = $slideshow.addEventListener('click', event => {
+	$backdrop.classList.remove('hide');
+	event.preventDefault();
+});
+
+$nextButton.addEventListener('click', () => {
+	if (counter !== 12) {
+		counter = counter + 1;
+		assignImageSource(counter);
+	} else {
+		endSlideshow();
+	}
+});
+
+$prevButton.addEventListener('click', () => {
+	if (counter !== 1) {
+		counter = counter - 1;
+		assignImageSource(counter);
+	} else {
+		endSlideshow();
+	}
+});
+
+$closeButton.addEventListener('click', () => {
+	endSlideshow();
+});
+
+$backdrop.addEventListener('click', () => {
+	endSlideshow();
+});
